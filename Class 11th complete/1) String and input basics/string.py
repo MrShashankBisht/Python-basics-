8aecc85c99f25ba3dd6694be3eb56454f3d0d0b7
sanku9868 <shashank.singh2016.ss@gmail.com>
2019-07-21 22:17:01 +0530

    First class python programme Adding string basics, data type and input

---
+++ Class 11th complete/1) String and input basics/string.py
@@ -0,0 +1,23 @@
+print("hello world!! ")
+print('hello world!!')
+print('''hello world!! ''')
+
+# with the helo of # symble we can write comment 
+
+# best way to write any string is by '' or ""
+
+print("hello novice " + "programmer !!")        # concatination of string 
+
+string1 = "this is a programming language. "
+String2 = "which is very easy !"
+print(string1+String2)                      # concatination of string type variable 
+
+#note that what we can't do 
+# print('your number is ' +  2)     we can concate only similar type of data 
+
+# how to get the data type of any variable 
+print(type("hello novice"))
+print(type(300+1))
+print(type(40.89))
+print(type(False))
+
