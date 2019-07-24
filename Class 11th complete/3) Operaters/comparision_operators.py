460d0101164f46ac15fb044fa3a12b710e045f07
sanku9868 <shashank.singh2016.ss@gmail.com>
2019-07-24 22:55:35 +0530

    adding operaters in class 11th folder

---
+++ Class 11th complete/3) Operaters/comparision_operators.py
@@ -0,0 +1,12 @@
+print( 'a' == 'a')
+print( 'a' == 'abc')
+print( 'A' != 'a')
+print( 'abc' == 'ABC')
+
+print( chr(65))
+print(chr(97))
+
+# alternative method to find the character int value 
+
+print( ord('A'))
+print( ord('a'))
